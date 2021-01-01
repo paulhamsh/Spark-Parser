eee369de97456544472156ba92e4d93b60f02dbd
paulhamsh <p.w.hamshere@gmail.com>
2021-01-01 11:14:17 +0000

    Add files via upload

--- SparkClassNew/SparkAmpParser.py
+++ SparkClassNew/SparkAmpParser.py
@@ -1,20 +1,18 @@
 import serial
 import struct
+import socket
 
 from SparkReaderClass import *
-
-
-BAUD = 1000000
 
 
 def send_ack(seq, cmd):
     ack = bytes.fromhex("01fe000041ff17000000000000000000f001" +
                         "%0.2X" % seq + "0004" + "%0.2X" % cmd + "f7")
-    ser.write(ack)
+    cs.write(ack)
 
 
 rd_data = b''
-read_len = 800
+read_len = 1
 
 
 def get_block():
@@ -22,16 +20,16 @@ def get_block():
     
     a = -1
     while a == -1:
-        rd_data += ser.read(read_len)
+        rd_data += cs.recv(read_len)
         a = rd_data.find(b'\x01\xfe')
         if a >= 0:
             # found the start, trim of anything before that
             rd_data = rd_data [a:]
             while len(rd_data) < 7:
-                rd_data += ser.read(read_len)
+                rd_data += cs.recv(read_len)
             blk_len = rd_data[6]
             while len(rd_data) < blk_len:
-                rd_data += ser.read(read_len)
+                rd_data += cs.recv(read_len)
 
             res = rd_data[:blk_len]
             rd_data = rd_data[blk_len:]
@@ -94,73 +92,30 @@ def get_data():
     return resp
 
 
-def get_data2():
-    resp=[]
-    
-    blk = get_block()
 
-    blk_len = blk[6]
-    seq     = blk[18]
-    cmd     = blk[20]
-    sub_cmd = blk[21]
 
-  
-    chunks_left = 1
-    
-    if cmd == 0x01 and sub_cmd != 0x04:
-        # the app sent a message that needs a response
-        send_ack(seq, cmd)
-            
-    bytes_to_skip = 0
-    
-    if sub_cmd == 38 and blk_len > 23:
-        # acknowlegement at start of block from Spark - skip it
-        cmd     = blk[27]
-        sub_cmd = blk[28]
-        bytes_to_skip = 7
-        # but we have one more f7 to look for
-        chunks_left += 1
+SERVER_PORT = 2
+MY_SPARK = "08:EB:ED:4E:47:07"  # Change to address of YOUR Spark
 
-        print ("\n\n********** GOT AN ACK IN SPARK MULTI-CHUNK ********\n\n")
-            
-    if sub_cmd == 0x01:
-        # this is a multi-chunk message, so get number of chunks needed to read
-        num_chunks = blk[23 + bytes_to_skip]
-        chunks_left += num_chunks - 1
-
-    while chunks_left > 0:
-        count_chunk = 0
-        for a_byte in blk:
-            if a_byte == 0xf7:
-                count_chunk += 1
-        chunks_left -= count_chunk
-        resp.append(blk)
-        if chunks_left > 0:
-            blk = get_block()
-            send_ack(seq,cmd)
-       
-    return resp
-
-# This captures the bluetooth traffic sent on a serial port
-# I am using a ESP32 board to mimick the Spark amp and capture bluetooth traffic and relay it to the serial port
-# Can be altered to parse a bytearray or similar
-
-ser=serial.Serial("COM7", BAUD, timeout=0)
 
 reader = SparkReadMessage()
 
 
 try:
+    cs = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    cs.connect((MY_SPARK, SERVER_PORT))
+    print ("Connected successfully")
+    
     while True:
         dat = get_data()
         reader.set_message(dat)
-        reader.read_message()
+        s = reader.read_message()
         print ("================")
-        print (reader.python)
+        #print (reader.python)
         print (reader.text)
-        print (reader.raw)
+        #print (reader.raw)
         
 finally:
-    ser.close()
+    cs.close()
 
 
